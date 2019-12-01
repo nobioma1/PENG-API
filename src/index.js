89f0152afc28f367690b85910bbb0ea80cb8ac4d
Noble Obioma <nobioma1@gmail.com>
2019-12-01 21:33:15 +0100

    Connect database and start server after db is connected

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-require('dotenv').config();
+/* eslint-disable no-console */
 const server = require('./api/server');
+const { connectDb } = require('./api/db/connect');
 
 const PORT = process.env.PORT || 5000;
 
-// eslint-disable-next-line no-console
-server.listen(PORT, () => console.log(`Server Live @ Port ${PORT} 🚀`));
+connectDb()
+  .then(connected => {
+    if (connected) {
+      server.listen(PORT, () => console.log(`Server Live @ Port ${PORT} 🚀`));
+    } else {
+      throw new Error('Error Starting Server');
+    }
+  })
+  .catch(error => console.log(error));
