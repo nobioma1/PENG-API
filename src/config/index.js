76fc6af6026ec13826ad16df4404ab98cbde5bf3
Noble Obioma <nobioma1@gmail.com>
2019-12-15 10:47:22 +0100

    Create config file to hold all environment variabe

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  databaseURL: process.env.DATABASE_URL,
+  port: process.env.PORT || 5000,
+  secret: process.env.SECRET || 'Here!, Look no farther 😈',
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 /* eslint-disable no-console */
 const server = require('./api/server');
-const { connectDb } = require('./api/db/connect');
-
-const PORT = process.env.PORT || 5000;
+const { connectDb } = require('./config/dbConnect');
+const { port } = require('./config');
 
 connectDb()
   .then(connected => {
     if (connected) {
-      server.listen(PORT, () => console.log(`Server Live @ Port ${PORT} 🚀`));
+      server.listen(port, () => console.log(`Server Live @ Port ${port} 🚀`));
     } else {
       throw new Error('Error Starting Server');
     }
