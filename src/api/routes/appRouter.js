eb79585027926d41a4a8817f4e921bb7409566c3
Noble Obioma <nobioma1@gmail.com>
2019-12-28 23:33:43 +0100

    Add workspace router and user router to app route after authToken middleware

--- src/api/routes/appRouter.js
+++ src/api/routes/appRouter.js
@@ -1,9 +1,12 @@
 const appRouter = require('express').Router();
 
+const { validateAuthToken } = require('../../resources/auth/authMiddleware');
 const authRouter = require('../../resources/auth/authRouter');
 const userRouter = require('../../resources/users/userRouter');
+const workspaceRouter = require('../../resources/workspaces/workspaceRouter');
 
 appRouter.use('/auth', authRouter);
-appRouter.use('/users', userRouter);
+appRouter.use('/user', validateAuthToken, userRouter);
+appRouter.use('/workspace', validateAuthToken, workspaceRouter);
 
 module.exports = appRouter;
