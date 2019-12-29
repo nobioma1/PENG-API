08b849b3e701bb194cd2163e4fee5354f1f2e2fd
Noble Obioma <nobioma1@gmail.com>
2019-12-29 12:34:25 +0100

    Export Measurement as Schema not model

---
+++ src/models/MeasurementSchema.js
@@ -0,0 +1,67 @@
+/* eslint-disable func-names */
+const mongoose = require('mongoose');
+
+const MeasurementSchema = new mongoose.Schema(
+  {
+    shoulder: {
+      type: Number,
+    },
+    aroundArm: {
+      type: Number,
+    },
+
+    Sleeve: {
+      type: Number,
+    },
+    Cuff: {
+      type: Number,
+    },
+    Neck: {
+      type: Number,
+    },
+    topLength: {
+      type: Number,
+    },
+    seatWaist: {
+      type: Number,
+    },
+    thighs: {
+      type: Number,
+    },
+    trouserLength: {
+      type: Number,
+    },
+    bar: {
+      type: Number,
+    },
+    chest: {
+      type: Number,
+    },
+    burst: {
+      type: Number,
+    },
+    dressLength: {
+      type: Number,
+    },
+    shoulderToUnderBust: {
+      type: Number,
+    },
+    shoulderToWaist: {
+      type: Number,
+    },
+    waist: {
+      type: Number,
+    },
+    hips: {
+      type: Number,
+    },
+    skirtLength: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = MeasurementSchema;
